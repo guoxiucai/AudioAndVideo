6af5af07905d851314515c922d4354f8533cca4c
GuoXiuCai <guoqingwei@cvte.com>
2023-07-05 14:18:47 +0800

    Delete NoiseSuppression directory

--- NoiseSuppression/sdk/cocoa/src/SWNoiseSuppression.h
+++
@@ -1,6 +0,0 @@
-#import <Foundation/Foundation.h>
-
-@interface SWNoiseSuppression : NSObject
-
-
-@end
--- NoiseSuppression/sdk/src/Commons/inc/Version.h
+++
@@ -1,3 +0,0 @@
-#pragma  once
-#define SDK_COMMIT_INFO "b0533f7826322d2ae51161f1534091dc4fc9fa4f"
-#define SDK_VERSION "1.0.0.9"
